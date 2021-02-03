fc717a43465037b736c1210119a1ddadd0f0f474
Sontus Chandra Anik <sontuschandraanik.cse@gmail.com>
2021-02-03 08:16:45 +0600

    Default and Template added

---
+++ defult.js
@@ -0,0 +1,6 @@
+function add(num1 = 0 , num2 = 0){
+    return num1 + num2;
+}
+
+const total = add(17,87);
+console.log(total);
---
+++ template.js
@@ -0,0 +1,5 @@
+const firstName = `Sontus Chandra Anik`;
+const multiLine = `Welcome to Bangladesh.
+We love our country.
+I working as a programmer.`;
+console.log(multiLine);
